42be77777216fdf736207944a6b26533e5e28834
Ram Ramaiyah <simplyram@Rams-MacBook-Pro.local>
2019-03-14 13:04:59 -0400

    Edit js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,13 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => {
-      return number;
-    }).join(', ');
+    if (this.state && this.state.seenIndexes && Array.isArray(this.state.seenIndexes)) 
+    {
+      return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    }
+    else {
+      return null;
+    }
   }
 
   renderValues() {
